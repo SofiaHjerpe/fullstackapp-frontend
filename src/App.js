a43e64ad1f899acb1f8cc34780071292d5e9affb
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-08-09 12:56:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [returnedData, setReturnedData] = useState(["hiii theeree"]);
+
+  const getData = async (url) => {
+    const newData = await fetch(url, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Accept: "application/json",
+      },
+    }).then((res) => res.json());
+    console.log(newData);
+    setReturnedData(newData.result);
+  };
+
+  const toggleMessage = () => {
+  returnedData === "Good Bye" ? getData("/api") : getData("/quit");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => toggleMessage()}>Click me</button>
+      {returnedData}
     </div>
   );
 }
